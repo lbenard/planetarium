32cce5c1f8afe7d61a55191e9360478e04449955
Lucas BENARD <lbenard@student.42.fr>
2021-10-08 22:45:17 +0200

    removed debug

--- stellar/stellar.gd
+++ stellar/stellar.gd
@@ -3,6 +3,3 @@ class_name Stellar
 
 var procedural_seed: int;
 var resources_set: ResourcesSet = null;
-
-func _ready():
-	print_debug("Stellar " + name + " has resources set: " + str(resources_set));
